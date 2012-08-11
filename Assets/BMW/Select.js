0ec8142f2bd7ab0e0bd62dd24b38bf2db7a0e6bf
Howard Stearns <howard.stearns@gmail.com>
2012-08-11 15:37:30 -0700

    Progress on drag stability. Still need to imporve curvey normals (like sphere).

--- Assets/BMW/Select.js
+++ Assets/BMW/Select.js
@@ -91,6 +91,7 @@ function hitNormal(hit:RaycastHit) {
 }
 
 public var pivotPrefab:Transform;
+public var rt:Vector3;
 function Update () {
     var hit:RaycastHit;
     var pointerRay:Ray;
@@ -112,6 +113,7 @@ function Update () {
 			contact.z = 0;
 			offset = contact - Input.mousePosition;
 			lastDragPosition = hit.point;
+			rt = selected.transform.TransformDirection(Vector3.right);
 			var pivot = Instantiate(pivotPrefab, hit.point, selected.transform.rotation);
 			Debug.Log("selected=" + selected + " parent=" + selected.transform.parent + " pivot=" + pivot);
 			pivot.parent = selected.transform.parent;
@@ -132,29 +134,31 @@ function Update () {
 			var trans:Transform = selected.transform.parent;
 			trans.Translate(delta, Space.World);
 			var norm:Vector3 = hitNormal(hit);
-			//var fwd:Vector3 = trans.forward;
-			//var up:Vector3 = trans.up;
-			//var proj:Vector3 = fwd - (Vector3.Dot(fwd, hit.normal)) * hit.normal;
+			//var fwdx:Vector3 = trans.forward;
+			//var upx:Vector3 = trans.up;
+			//var proj:Vector3 = fwdx - (Vector3.Dot(fwdx, hit.normal)) * hit.normal;
 			//var angle = 0.0; var axis = Vector3.zero; transform.rotation.ToAngleAxis(angle, axis); 
 			
-			var fwd2:Vector3 = trans.TransformDirection(Vector3.forward);
-			var up2:Vector3 = trans.TransformDirection(Vector3.up);
-			Debug.DrawRay(hit.point, fwd2, Color.blue);
-			Debug.DrawRay(hit.point, up2, Color.green);
-			Debug.DrawRay(hit.point, norm, Color.cyan);
-			if (Mathf.Abs(Vector3.Dot(fwd2, norm)) > 0.5) {
+			var up:Vector3 = norm; //trans.TransformDirection(Vector3.up);
+			var fwd:Vector3 = Mathf.Abs(Vector3.Dot(rt, up)) > 0.9 
+				? trans.TransformDirection(Vector3.forward)
+				: Vector3.Cross(rt, up);
+			Debug.DrawRay(hit.point, fwd, Color.blue);
+			Debug.DrawRay(hit.point, up, Color.green);
+
+			/*if (Mathf.Abs(Vector3.Dot(fwd2, norm)) > 0.5) {
 				var tmp:Vector3 = fwd2;
 				fwd2 = up2;
 				up2 = tmp;
-			}
+			}*/
 			var rot:Quaternion = 
 				//Quaternion.AngleAxis(angle, norm);  // Bad.
-				//Quaternion.LookRotation(fwd, norm);  // Too stable. Preserves forward forward and up second.
+				//Quaternion.LookRotation(fwdx, norm);  // Too stable. Preserves forward forward and up second.
 				//Quaternion.LookRotation(Vector3.forward, norm); // Too stable.
 				//Quaternion.LookRotation(proj, norm); // Mostly like trans.up = norm, below, but not not really.
-				//Quaternion.FromToRotation(up, norm);  // Very jittery
+				//Quaternion.FromToRotation(upx, norm);  // Very jittery
 				//Quaternion.FromToRotation(Vector3.up, norm); // Works, but sometimes has rotation around local z.
-				Quaternion.LookRotation(fwd2, up2);
+				Quaternion.LookRotation(fwd, up);
 			trans.rotation = rot;
 			//trans.up = norm;  // same as using FromToRotation(Vector3.up, norm), above.
 			

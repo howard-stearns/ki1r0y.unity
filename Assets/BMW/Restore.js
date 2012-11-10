4493440545679442c54f5428dce3c34e56c83bca
Howard Stearns <howard.stearns@gmail.com>
2012-11-10 10:21:38 -0800

    Checkpoint.

---
+++ Assets/BMW/Restore.js
@@ -0,0 +1,88 @@
+/* Todo:
+	Real save, as needed.
+	Real db.
+	Textures.
+	Make code more data-driven.
+	What to do about change history, GC?
+ */
+	
+
+public var db = 
+{
+"24faf820e829988a6b831d15ec4c25220bc7971e": '{"name" : "backWall","type" : "Plane"}',
+"d307898c060a157923211768ee1c09da6b07b00c": '{"name" : "Directional light", "type" : "Directional", "intensity" : 0.5}',
+"441c5fb6468a649eaa83db1dc3d09d8b67077b0e": '{"name" : "floor","type" : "Plane"}',
+"e85b09aa48f25c0e0436729f3334a29dcf343cd8": '{"name" : "leftWall","type" : "Plane"}',
+"d5c9358048b484a083423e09d5bc3f32ada45622": '{"name" : "rotatedCube","type" : "Cube"}',
+"665c7030bab074f9f6a455003eda1f40b4b72009": '{"name" : "Sphere","type" : "Sphere"}',
+"da25a18cd08161823d3bcb1e295bf8ae843db65f": '{"name" : "tallCube","type" : "Cube"}',
+"419d57d1fc5657815cd997fc566910e62a9830f9": 
+ '{"children" : [{"id" : "24faf820e829988a6b831d15ec4c25220bc7971e","position" : {"x" : 8.450162,"y" : -0.0377996,"z" : 4.243648},"rotation" : {"w" : 0.7071067,"x" : 0,"y" : 0,"z" : 0.7071068}},{"id" : "d307898c060a157923211768ee1c09da6b07b00c","position" : {"x" : 2.200287,"y" : 0.6813452,"z" : 2.953255},"rotation" : {"w" : 0.9116248,"x" : 0.08803673,"y" : 0.2966023,"z" : -0.2705861}},{"id" : "441c5fb6468a649eaa83db1dc3d09d8b67077b0e","position" : {"x" : 3.470267,"y" : -2.294606,"z" : 4.272788}},{"id" : "e85b09aa48f25c0e0436729f3334a29dcf343cd8","position" : {"x" : 3.488311,"y" : -0.9996749,"z" : 9.101078},"rotation" : {"w" : 0.7071067,"x" : -0.7071068,"y" : 0,"z" : 0}},{"id" : "d5c9358048b484a083423e09d5bc3f32ada45622","position" : {"x" : 5.909942,"y" : -1.832091,"z" : 3.089984},"rotation" : {"w" : 0.9008605,"x" : 0,"y" : -0.4341087,"z" : 0}},{"id" : "665c7030bab074f9f6a455003eda1f40b4b72009","position" : {"x" : 0.9262815,"y" : -1.153377,"z" : 5.923676},"scale" : {"x" : 2,"y" : 2,"z" : 2}},{"id" : "da25a18cd08161823d3bcb1e295bf8ae843db65f","position" : {"x" : 3.50061,"y" : -1.294861,"z" : 4.245239},"scale" : {"x" : 1,"y" : 2,"z" : 1}}],"name" : "TestStage"}'
+};
+
+
+function makeVector3(data:Hashtable):Vector3 {
+	return Vector3(data['x'], data['y'], data['z']);
+}
+function makeQuaternion(data:Hashtable):Quaternion {
+	return Quaternion(data['x'], data['y'], data['z'], data['w']);
+}
+
+function Restore(id:String):GameObject {
+	Debug.Log('temp ' + id);
+	var temp = GameObject.CreatePrimitive(PrimitiveType.Cube);
+	Inflate(temp, id);
+	return temp;
+}
+
+function Inflate(temp:GameObject, id:String) {
+    //yield WaitForSeconds(2);
+	//var serialization:String = db[id]; // simulation of server fetch
+	var www = new WWW('http://beyondmywall.fe100.net/db/' + id);
+	//var www = new WWW('file:///Users/howardstearns/Beyond-My-Wall/server/BMW/public/db/' + id);
+    yield www;
+   	var serialization = www.text;
+	var data:Hashtable = JSON.Parse(serialization);
+	Debug.Log('Inflating ' + data['name'] + ' into ' + id);
+	var go:GameObject;
+	var type:String = data['type'];
+	var pt:Object = null;
+	if (type == 'Plane') pt = PrimitiveType.Plane;
+	else if (type == 'Cube') pt = PrimitiveType.Cube;
+	else if (type == 'Sphere') pt = PrimitiveType.Sphere;
+	if (pt != null) go = GameObject.CreatePrimitive(pt);
+	else {
+		go = new GameObject();
+		if (type == 'Directional') pt = LightType.Directional;
+		else if (type == 'Point') pt = LightType.Point;
+		else if (type === 'Spot') pt = LightType.Spot;
+		if (pt != null) {
+			var light = go.AddComponent(Light);
+			light.type = pt;
+			light.intensity = data['intensity'];
+		} 
+	}
+	go.name = data['name'];
+	for (var childData:Hashtable in data['children']) {
+		var child = Restore(childData['id']);  
+		// At this point, both go and child are at top level, with no parent transform
+		var pos = childData['position'];
+		if (pos != null) child.transform.localPosition = makeVector3(pos);
+		var rot = childData['rotation'];
+		if (rot != null) child.transform.localRotation = makeQuaternion(rot);
+		var scale = childData['scale'];
+		if (scale != null) child.transform.localScale = makeVector3(scale);
+		child.transform.parent = go.transform;
+	}
+	// Now replace the temp with our new go.
+	go.transform.position = temp.transform.position;
+	go.transform.rotation = temp.transform.rotation;
+	go.transform.localScale = temp.transform.localScale;
+	go.transform.parent = temp.transform.parent;
+	temp.transform.parent = null;
+	Destroy(temp);
+}
+
+function Awake () {
+	Restore("419d57d1fc5657815cd997fc566910e62a9830f9");
+}
---
+++ Assets/BMW/Save.js
@@ -0,0 +1,83 @@
+function sha1(serialized:String) {
+	var sb = new System.Text.StringBuilder();
+	var bytes = System.Text.Encoding.ASCII.GetBytes(serialized);
+  	for (var byteChunk in System.Security.Cryptography.SHA1.Create().ComputeHash(bytes)) 
+    	sb.Append(byteChunk.ToString("x2"));
+  	return sb.ToString();
+}
+function uploadData(hash:String, serialized:String) {
+	// Must be separate void function to be yieldable.
+ 	Debug.Log(hash + ': ' + serialized); // simulated upload
+ 	var form = new WWWForm();
+	form.AddField('data', serialized);
+	var www = WWW('http://beyondmywall.fe100.net/db/' + hash, form);
+	yield www;
+	if (www.error) print('upload ' + hash + ' failed ' + www.error);
+	else Debug.Log(www.text);
+}
+function store(data:Hashtable):String { // answer hash
+	var serialized = JSON.Stringify(data);
+	var hash = sha1(serialized);
+	uploadData(hash, serialized);
+  	return hash;
+}
+
+function Persist(x:GameObject):Hashtable {
+	var shared = new Hashtable(); // Common data for all instances of this object.
+	AddProperty(shared, 'name', x.name);
+	// The component var is deliberately untyped, else the compiler will
+	// be too clever by half and ALWAYS pick the Component overload!
+	for (var component:Object in x.GetComponents(typeof Component)) {
+		AddComponent(shared, component);
+	}
+	var hash = store(shared);
+	// Report only this particular instance data to caller.
+	var instance = new Hashtable(); 
+	AddProperty(instance, 'id', hash);
+	if (x.transform.localPosition != Vector3.zero) 
+		AddProperty(instance, 'position', x.transform.localPosition);
+	if (x.transform.localRotation != Quaternion.identity)
+		AddProperty(instance, 'rotation', x.transform.localRotation);
+	if (x.transform.localScale != Vector3.one)
+		AddProperty(instance, 'scale', x.transform.localScale);
+	return instance;
+}
+
+function AddProperty (p:Hashtable, key:String, x) {
+	p[key] = x;
+}
+function AddProperty (p:Hashtable, key:String, v:Vector3) {
+	AddProperty(p, key, {'x':v.x, 'y':v.y, 'z':v.z});
+}
+function AddProperty (p:Hashtable, key:String, q:Quaternion) {
+	AddProperty(p, key, {'x':q.x, 'y':q.y, 'z':q.z, 'w':q.w});
+}
+
+function AddComponent(p:Hashtable, component:Transform) {
+	// The only shared data for all instances is the child data.
+	// The instance-specific transform data (position/rotation/scale) is above.
+	var children:Array = [];
+	for (var child:Transform in component) {
+		var persisted:Hashtable = Persist(child.gameObject);
+		if (persisted.Count != 0)
+			children.Push(persisted);
+	}
+	if (children.length != 0)
+		AddProperty(p, 'children', children);
+}
+function AddComponent(p:Hashtable, component:MeshFilter) {
+	AddProperty(p, 'type', component.sharedMesh.name);
+}
+// Todo: Renderer: list materials[n].mainTexture
+function AddComponent(p:Hashtable, component:Light) {
+	AddProperty(p, 'type', component.type);
+	AddProperty(p, 'instensity', component.intensity.ToString());
+}
+function AddComponent(p:Hashtable, component:Component) {
+}
+
+function Start () {
+	yield WaitForSeconds(4);
+	var p = Persist(gameObject);
+	store(p);
+}

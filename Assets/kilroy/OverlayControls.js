2f638822b4e80a4a1dc3cc4b7f2e8fc1b2f0fd0d
Howard Stearns <howard.stearns@gmail.com>
2012-12-09 18:30:31 -0800

    Restore mouse movement on click (e.g., after escape, to reposition mouse).

--- Assets/kilroy/OverlayControls.js
+++ Assets/kilroy/OverlayControls.js
@@ -1,17 +1,15 @@
 // Turns off mouse look when the mouse leaves the player.
 // Attach this script to a GUITexture that covers the player, but which has no texture.
 public var looks:MouseLook[];
-
-// Doesn't work on iPhone, but neither is it needed.
-function OnMouseEnter() {
-    for (var script:MouseLook in looks) script.enabled = true;
-}
-function OnMouseExit() {
-    for (var script:MouseLook in looks) script.enabled = false;
+function trackMouseMotion(enabled:boolean) {
+	for (var script:MouseLook in looks) script.enabled = enabled;
 }
 
+// Note that iPhone doesn't have OnMouse<mumble>.
+function OnMouseEnter() { trackMouseMotion(true); }
+function OnMouseDown() { trackMouseMotion(true); }
+
+function OnMouseExit() { trackMouseMotion(false); }
 function Update() {
-	if (Input.GetKeyUp(KeyCode.Escape)) {
-		for (var script:MouseLook in looks) script.enabled = false;
-	}
+	if (Input.GetKeyUp(KeyCode.Escape)) trackMouseMotion(false);
 }

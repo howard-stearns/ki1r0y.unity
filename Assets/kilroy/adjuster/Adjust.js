f715e26c453be65312a62ad5d9b10f34d75c5ed1
Howard Stearns <howard.stearns@gmail.com>
2013-10-09 19:55:27 -0700

    Stable corner adjusters that work. (Still needs rotation.)

--- Assets/kilroy/adjuster/Adjust.js
+++ Assets/kilroy/adjuster/Adjust.js
@@ -1,81 +1,58 @@
 class Adjust extends Directional {
 
-static function projectPointOnPlane(point, planeNormal, planePoint) {
-	return point - (planeNormal * Vector3.Dot(planeNormal, point - planePoint));
-}
-// divide each component of n by d. If d component is zero, use defaultComponent.
+// Not used. divide each component of n by d. If d component is zero, use defaultComponent. 
 static function div(n:Vector3, d:Vector3, defaultComponent:float) {
 	return Vector3(d.x ? n.x / d.x : defaultComponent,
 					d.y ? n.y / d.y : defaultComponent,
 					d.z ? n.z / d.z : defaultComponent);
 }
+static function projectPointOnPlane(point, planeNormal, planePoint) {
+	return point - (planeNormal * Vector3.Dot(planeNormal, point - planePoint));
+}
 static function noFlip(scale:Vector3) { // Make sure that scale doesn't flip
 	return Vector3(Mathf.Abs(scale.x), Mathf.Abs(scale.y), Mathf.Abs(scale.z));
 }
 
-// These are in global coordinates
-var centerPlaneFirstCorner:Vector3; 
-var firstPoint:Vector3;
+var assemblyObj:Obj;
 // These are in our coordinate system.
-var firstAffordanceLocalPosition:Vector3;
-var firstAffordanceLocalScale:Vector3;
-
-function updateAffordance() { //As we scale assembly during movement, invert that to keep affordance at constant size in corners.
-	affordanceCollider.transform.localPosition = div(firstAffordanceLocalPosition, noFlip(axis.localRotation * assembly.localScale), 1);
-	//affordanceCollider.transform.localScale = div(firstAffordanceLocalScale, Vector3(assembly.localScale.y, 1, assembly.localScale.z), 1);
-	affordanceCollider.transform.localScale = div(firstAffordanceLocalScale, noFlip(axis.localRotation * affordanceCollider.transform.localRotation * assembly.localScale), 1);
+public var cornerUnitPositionFromAxis:Vector3;
+public var flipX = false;
+function updateAffordance() { //As we resize assembly during movement, keep affordance at constant size in new corners.
+	var pos = Vector3.Scale(cornerUnitPositionFromAxis, axis.localRotation * assemblyObj.size());
+	// I don't know why this needs adjusting, but it does.
+	if (flipX) { pos.x = -pos.x; }
+	transform.localPosition = pos;
 }
-
 function Start() {
 	super.Start();
-	var aff = transform.Find('affordance');
-	affordanceCollider = aff.collider;
-	firstLocalPosition = transform.localPosition;
-	firstAffordanceLocalPosition = aff.localPosition;
-	firstAffordanceLocalScale = aff.localScale;
-//	centerPlaneFirstCorner = projectPointOnPlane(transform.position, axis.right, axis.position);
-//	Debug.Log(transform.parent + ' rotated:' + (axis.localRotation * aff.localRotation * Vector3(1, 2, 3)));
+	flipX = Vector3.Dot(axis.right, assembly.up) > 0.2;
+	assemblyObj = assembly.gameObject.GetComponent.<Obj>();
+	affordanceCollider = transform.Find('affordance').collider;
 }
-function Update() {
-	super.Update();
-	updateAffordance();
-}
+
+// In world coordinates.
+public var firstPoint:Vector3;
+public var startCorner:Vector3;      // The "corner" offset to a plane (parallel to the interaction plane) that runs through the middle of the assembly.
+public var oppositeCorner:Vector3;   // The opposite "corner" (on that same middle plane).
+public var orthogonalCorner:Vector3; // Not really a corner, it extends a half width past the startCorner in the middle. Length is the assembly dimension perpendicular to plane.
 
 function startDragging(assembly:Transform, axis:Transform, plane:Transform, cameraRay:Ray, hit:RaycastHit) {
 	plane.rotation = affordanceCollider.transform.rotation;
 	plane.position = transform.position;
 	firstPoint = hit.point;
-	centerPlaneFirstCorner = (projectPointOnPlane(transform.position, axis.right, axis.position) - axis.position);
-	Debug.Log('hit:' + (hit.point * 10) + ' corner:' + (transform.position * 10) + ' centerPlane:' + (centerPlaneFirstCorner * 10));
+	startCorner = projectPointOnPlane(transform.position, axis.right, axis.position);
+	oppositeCorner = axis.position - (startCorner - axis.position);
+	orthogonalCorner = Vector3.Project(assemblyObj.size(), assembly.InverseTransformDirection(axis.right));
 	return plane.collider;
 }
-/*
-	At the start of a drag, each assembly axis has a scale of 1. (See Directional.ApplyChanges().)
-	For each assembly axis during a resizing drag, with o = assembly origin, s = starting extremity, n = new:
-   	o        s  n
-   	Scale = n/s.     (We should construct things so that s cannot be zero.)
-*/
 function doDragging(assembly:Transform, axis:Transform, plane:Transform, hit:RaycastHit) {
 	var pointerDelta = hit.point - firstPoint;
-	var newCorner = centerPlaneFirstCorner + pointerDelta;
-	var ratio = div(newCorner, centerPlaneFirstCorner, 1.0);
-	assembly.localScale = noFlip(Quaternion.Inverse(axis.localRotation) * ratio); 
-//	Debug.Log('hit:' + (hit.point * 10) + ' delta:' + (pointerDelta * 10) + ' new:' + (newCorner * 10) + ' ratio:' + (ratio * 10) + ' scale:' + (assembly.localScale * 10));
-//	updateAffordance();
+	var newCorner = startCorner + pointerDelta;
+	assembly.position = (newCorner + oppositeCorner) / 2;
+	var extent = noFlip(assembly.InverseTransformDirection(newCorner - oppositeCorner) + orthogonalCorner);
+	assemblyObj.size(extent);
+	assembly.BroadcastMessage('updateAffordance', null, SendMessageOptions.DontRequireReceiver);
 //	v = Vector3.Scale(assembly.localScale, assembly.GetComponent.<Obj>().size());
 //	Application.ExternalCall('updateSize', v.x, v.y, v.z);
-}
-function resetAffordance(assy:Transform) {
-	//transform.localPosition = Vector3.Scale(transform.localPosition, assy.localScale);
-	transform.localPosition = Vector3.Scale(transform.localPosition, noFlip(axis.localRotation * assy.localScale));
-}
-function stopDragging(assy:Transform):Obj{
-	axis.parent.BroadcastMessage('resetAffordance', assy, SendMessageOptions.DontRequireReceiver);
-	return super.stopDragging(assy);
-//	var s = assy.localScale;
-//	var o = super.stopDragging(assy);
-//	transform.localPosition = Vector3.Scale(transform.localPosition, s);
-//	updateAffordance();
-//	return o;
 }
 }

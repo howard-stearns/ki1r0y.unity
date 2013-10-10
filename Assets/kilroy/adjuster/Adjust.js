ce706a2aea6c7b0b415c15e5c1525a3fbbfab9a0
Howard Stearns <howard.stearns@gmail.com>
2013-10-09 21:30:05 -0700

    Corner adjusters now do rotations as well as size. (Both relative to opposite corner.)

--- Assets/kilroy/adjuster/Adjust.js
+++ Assets/kilroy/adjuster/Adjust.js
@@ -25,11 +25,13 @@ function updateAffordance() { //As we resize assembly during movement, keep affo
 }
 function Start() {
 	super.Start();
-	flipX = Vector3.Dot(axis.right, assembly.up) > 0.2;
+	flipX = Vector3.Dot(axis.right, assembly.right) < 0.2;
 	assemblyObj = assembly.gameObject.GetComponent.<Obj>();
 	affordanceCollider = transform.Find('affordance').collider;
 }
-
+public var doRotate = false;
+public var lastRotationV:Vector3;
+public var rotationCenter:Vector3;
 // In world coordinates.
 public var firstPoint:Vector3;
 public var startCorner:Vector3;      // The "corner" offset to a plane (parallel to the interaction plane) that runs through the middle of the assembly.
@@ -37,20 +39,32 @@ public var oppositeCorner:Vector3;   // The opposite "corner" (on that same midd
 public var orthogonalCorner:Vector3; // Not really a corner, it extends a half width past the startCorner in the middle. Length is the assembly dimension perpendicular to plane.
 
 function startDragging(assembly:Transform, axis:Transform, plane:Transform, cameraRay:Ray, hit:RaycastHit) {
+	doRotate = !!Input.GetAxis('Fire2');
 	plane.rotation = affordanceCollider.transform.rotation;
 	plane.position = transform.position;
 	firstPoint = hit.point;
 	startCorner = projectPointOnPlane(transform.position, axis.right, axis.position);
 	oppositeCorner = axis.position - (startCorner - axis.position);
 	orthogonalCorner = Vector3.Project(assemblyObj.size(), assembly.InverseTransformDirection(axis.right));
+	rotationCenter = projectPointOnPlane(oppositeCorner, axis.right, plane.position);
+	lastRotationV = firstPoint - rotationCenter;
 	return plane.collider;
 }
 function doDragging(assembly:Transform, axis:Transform, plane:Transform, hit:RaycastHit) {
-	var pointerDelta = hit.point - firstPoint;
-	var newCorner = startCorner + pointerDelta;
-	assembly.position = (newCorner + oppositeCorner) / 2;
-	var extent = noFlip(assembly.InverseTransformDirection(newCorner - oppositeCorner) + orthogonalCorner);
-	assemblyObj.size(extent);
+	if (doRotate) {
+		var v = hit.point - rotationCenter; 
+		var angle = Vector3.Angle(lastRotationV, v);
+		// but angle is always the smallest positive value. We need to determine direction...
+		var cross = Vector3.Cross(lastRotationV, v);
+		if (Vector3.Dot(cross, axis.right) < 0.0) { angle = -1 * angle; }
+		assembly.transform.RotateAround(rotationCenter, axis.right, angle);
+		lastRotationV = v;
+	} else {
+		var pointerDelta = hit.point - firstPoint;
+		var newCorner = startCorner + pointerDelta;
+		assembly.position = (newCorner + oppositeCorner) / 2;
+		assemblyObj.size(noFlip(assembly.InverseTransformDirection(newCorner - oppositeCorner) + orthogonalCorner));
+	}
 	assembly.BroadcastMessage('updateAffordance', null, SendMessageOptions.DontRequireReceiver);
 //	v = Vector3.Scale(assembly.localScale, assembly.GetComponent.<Obj>().size());
 //	Application.ExternalCall('updateSize', v.x, v.y, v.z);
